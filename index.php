f98978eb2d630f2b78579cab7eab26e71f576f67
Jamal Guyo <jamalguyo08@gmail.com>
2018-01-18 10:30:41 +0300

    original file

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <meta type = "viewport" content="width=device-width, initial-scale=1.0, user-scalable=no"/>
+        <title>fullscreen</title>
+        <link rel="stylesheet" type="text/css" href="style.css"/>
+        
+    </head>
+    <body>   
+        
+        <section class="b1">
+            <div class="in1">
+                <div class="content">
+                    <h1>Dark Code</h1>
+                    <a href="#">Get Started</a>
+                    
+                </div>
+            </div>
+        </section>
+        <p>
+            lorem ipsum sample text here sample lorem lorem ipsum sample text here sample lorem lorem ipsum sample text here sample loremlorem ipsum sample text here sample loremlorem ipsum sample text here sample loremlorem ipsum sample text here sample loremlorem ipsum sample text here sample loremlorem ipsum sample text here sample lorem
+        </p>
+        <p>
+            lorem ipsum sample text here sample lorem lorem ipsum sample text here sample lorem lorem ipsum sample text here sample loremlorem ipsum sample text here sample loremlorem ipsum sample text here sample loremlorem ipsum sample text here sample loremlorem ipsum sample text here sample loremlorem ipsum sample text here sample lorem
+        </p>
+        <p>
+            lorem ipsum sample text here sample lorem lorem ipsum sample text here sample lorem lorem ipsum sample text here sample loremlorem ipsum sample text here sample loremlorem ipsum sample text here sample loremlorem ipsum sample text here sample loremlorem ipsum sample text here sample loremlorem ipsum sample text here sample lorem
+        </p>
+        <p>
+            lorem ipsum sample text here sample lorem lorem ipsum sample text here sample lorem lorem ipsum sample text here sample loremlorem ipsum sample text here sample loremlorem ipsum sample text here sample loremlorem ipsum sample text here sample loremlorem ipsum sample text here sample loremlorem ipsum sample text here sample lorem
+        </p>
+        <p>
+            lorem ipsum sample text here sample lorem lorem ipsum sample text here sample lorem lorem ipsum sample text here sample loremlorem ipsum sample text here sample loremlorem ipsum sample text here sample loremlorem ipsum sample text here sample loremlorem ipsum sample text here sample loremlorem ipsum sample text here sample lorem
+        </p>
+    </body>
+</html>
